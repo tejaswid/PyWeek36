50c9261ece2535aa61be77944c4a04f5c2c39022
KrishnaManaswiD <dkrishnamanaswi@gmail.com>
2023-09-17 22:50:22 +1000

    implemented acceleration based control

--- TheGame/modules/player.py
+++ TheGame/modules/player.py
@@ -20,29 +20,37 @@ class Player(GameObject):
         self.mouse_handler = mouse.MouseStateHandler()
         self.event_handlers = [self, self.key_handler, self.mouse_handler]
 
-        self.speed = 1
-        self.velocity = [self.speed,self.speed]
+        self.speed = 100
+        self.velocity = [self.speed,0]
+        self.acceleration_magnitude = 500
 
 
     def update_object(self, dt):
         if self.key_handler[key.A]:
-            self.speed = 3 # small boost
-            self.update_velocity()
-        if self.key_handler[key.RIGHT]:
-            self.rotation += 10 * dt
+            self.update_velocity(dt)
         self.update_position(dt)
         
 
     # updates the position of the player
     def update_position(self,dt):
-        self.x += self.velocity[0]
-        self.y += self.velocity[1]
+        self.x += self.velocity[0] * dt
+        self.y += self.velocity[1] * dt
 
     # update rotation of the player based on mouse position
     def update_rotation(self, mouse_x, mouse_y):
         self.rotation = math.atan2(mouse_y-self.y, mouse_x-self.x) * -180 / math.pi
 
     # update the velocity of the player - new velocity vector must be aligned with rotation
-    def update_velocity(self):
-        self.velocity[0] = math.cos(self.rotation * math.pi / 180)
-        self.velocity[1] = -math.sin(self.rotation * math.pi / 180)
+    def update_velocity(self, dt):
+        self.velocity[0] += self.acceleration_magnitude * math.cos(self.rotation * math.pi / 180) * dt
+        self.velocity[1] += self.acceleration_magnitude * -math.sin(self.rotation * math.pi / 180) * dt
+
+        if self.velocity[0] > self.speed:
+            self.velocity[0] = self.speed
+        if self.velocity[0] < -self.speed:
+            self.velocity[0] = -self.speed
+        if self.velocity[1] > self.speed:
+            self.velocity[1] = self.speed
+        if self.velocity[1] < -self.speed:
+            self.velocity[1] = -self.speed
+        
