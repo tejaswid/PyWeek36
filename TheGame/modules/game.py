da47472102288e1759256084fe4530bdb1521090
KrishnaManaswiD <dkrishnamanaswi@gmail.com>
2023-09-17 18:31:29 +1000

    added basic game window

--- TheGame/modules/game.py
+++ TheGame/modules/game.py
@@ -2,3 +2,12 @@ import pyglet
 
 def run():
     print(pyglet.version)
+
+    # create the game window - size is 1000px x 1000px
+    window = pyglet.window.Window(1000, 1000, "The game", resizable=False)
+
+    @window.event
+    def on_draw():
+        window.clear()
+    
+    pyglet.app.run()
