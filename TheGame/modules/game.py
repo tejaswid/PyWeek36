1a57c12666419fa34e049e40a37c51095e44eec9
KrishnaManaswiD <dkrishnamanaswi@gmail.com>
2023-09-17 21:34:53 +1000

    added basic mouse and keyboard code. changed mouse to crosshair

--- TheGame/modules/game.py
+++ TheGame/modules/game.py
@@ -1,4 +1,6 @@
 import pyglet
+from pyglet.window import key
+from pyglet.window import mouse
 
 from modules.game_assets import GameAssets
 from modules.player import Player
@@ -24,11 +26,34 @@ def run():
     # list of all objects in the simulation
     game_objects = []
 
+    # change mouse cursor
+    cursor = window.get_system_mouse_cursor(window.CURSOR_CROSSHAIR)
+    window.set_mouse_cursor(cursor)
+
+    # drawing items on screen
     @window.event
     def on_draw():
         window.clear()
         main_batch.draw()
 
+
+    # handle keyboard inputs
+    @window.event
+    def on_key_press(symbol, modifiers):
+        if symbol == key.A:
+            print('The "A" key was pressed.')
+
+
+    # handle mouse inputs
+    @window.event
+    def on_mouse_press(x, y, button, modifiers):
+        if button == mouse.LEFT:
+            print('The left mouse button was pressed.')
+
+
+    @window.event
+    def on_mouse_motion(x, y, dx, dy):
+        print(x,y)
 
     # loads the main scene
     def load_main_scene():
